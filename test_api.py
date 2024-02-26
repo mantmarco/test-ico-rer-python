434f15c9b24978d42137eea195377e8d79a990de
mantmarco <51927352+mantmarco@users.noreply.github.com>
2024-02-26 18:42:31 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,11 +1,9 @@
-import os
+from fastapi import FastAPI
 
-def scrivi_file_di_prova(nome_file):
-    with open(nome_file, 'w') as file:
-        file.write("Questo è un file di prova.\n")
-        file.write("Puoi scrivere quello che vuoi qui.\n")
+app = FastAPI()
 
-if __name__ == "__main__":
-    nome_file = "test.txt"
-    scrivi_file_di_prova(nome_file)
-    print(f"Il file di prova '{nome_file}' è stato creato con successo.")
+@app.get("/")
+
+async def root():
+    return {"message": "Hello World"}
+    
